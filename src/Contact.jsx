f1b59aae7a6ee7a98e40b6779bb9c1d03838a0a9
Julie Ahlquist <julieahlquist@hotmail.com>
2019-06-26 14:08:39 +0200

    added Contacts section and img file

---
+++ src/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="content-wrapper">
+            <div className="flex flex-wrap mb-4">
+                <div className="w-full lg:w-3/4 md:w-1/2">
+                    <h1>Contacts</h1>
+                    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
