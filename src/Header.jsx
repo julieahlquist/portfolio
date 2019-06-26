9ccead7969a246314be7a7f857ce3fee178f722b
Julie Ahlquist <julieahlquist@hotmail.com>
2019-06-26 14:13:03 +0200

    minor changes of styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,12 +5,12 @@ const Header = () => {
     return (
         <nav className="header">
             <h1 className="header-title">
-                <Link className="text-white hover:text-grey no-underline" to='/'>My Portfolio</Link>
+                <Link className="text-white hover:text-grey-light no-underline" to='/'>My Portfolio</Link>
             </h1>
             <ul className="list-reset flex">
-                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/about'>About Me</NavLink></li>
-                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/projects'>My Projects</NavLink></li>
-                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/contact'>Contact</NavLink></li>
+                <li className="mr-6"><NavLink className="text-white hover:text-grey-light no-underline" to='/about'>About Me</NavLink></li>
+                <li className="mr-6"><NavLink className="text-white hover:text-grey-light no-underline" to='/projects'>My Projects</NavLink></li>
+                <li className="mr-6"><NavLink className="text-white hover:text-grey-light no-underline" to='/contact'>Contact</NavLink></li>
             </ul>
         </nav>
     )
