1913f4d8d92936e47993a306a3e37914abcbbbd4
Julie Ahlquist <julieahlquist@hotmail.com>
2019-06-28 14:46:49 +0200

    first projectcard links and styled buttons

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -14,11 +14,11 @@ const ProjectCard = (props) => {
                 </p>
             </div>
 
-            <div className="flex items-center justify-between leading-none p-2 md:p-4">
-                <button className="bg-blue-darkest hover:bg-blue-dark text-white text-xs py-1 px-2 rounded-full">
-                    Details
-                </button>
+            <div className="btn-wrapper">
+                <a href={project.website} target="_blank" className="btn">Website</a>
+                <a href={project.github} target="_blank" className="btn">GitHub</a>
             </div>
+
 
         </div>
     )
